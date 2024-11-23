bf010bfaa4d6786330eb0916dfa54a8c4d1f8bcd
Svyatoslav Pershko <88134893+Svyatoy1@users.noreply.github.com>
2024-11-23 23:26:56 +0000

    updated react files, particulary progress bar on to-do-page and css file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,22 +6,23 @@ import PomodoroTimer from './PomodoroTimer';
 import './App.css';
 
 const App = () => {
-    return (
-        <Router>
-            <div className="wrapper">
-                <nav className="navbar">
-                    <a href="/">Register</a>
-                    <a href="/todo">To-Do List</a>
-                    <a href="/pomodoro">Pomodoro Timer</a>
-                </nav>
-                <Routes>
-                    <Route path="/" element={<RegistrationForm />} />
-                    <Route path="/todo" element={<TodoList />} />
-                    <Route path="/pomodoro" element={<PomodoroTimer />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">To-Do List</Link></li>
+          <li><Link to="/pomodoro">Pomodoro Timer</Link></li>
+          <li><Link to="/register">Register</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<TodoList />} />
+        <Route path="/pomodoro" element={<PomodoroTimer />} />
+        <Route path="/register" element={<RegistrationForm />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
